892f0b94fed998ca340589493e23227d07ae332a
fooltrader <buddha8811@gmail.com>
2026-03-24 10:24:49 +0800

    Update sync-rss.js

--- sync-rss.js
+++ sync-rss.js
@@ -5,7 +5,12 @@ const got = require('got');
 const jsdom = require("jsdom");
 const {JSDOM} = jsdom;
 const Parser = require('rss-parser');
-const parser = new Parser();
+const parser = new Parser({
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+  }
+});
 const {DB_PROPERTIES, PropertyType, sleep} = require('./util');
 
 config();
